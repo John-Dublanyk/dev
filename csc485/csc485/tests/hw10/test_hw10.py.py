70e81436caabbeb5493ed98b186b89011f8a5427
John-Dublanyk <jdubl001@plattsburgh.edu>
2023-10-03 23:32:51 -0400

    Initial commit

---
+++ csc485/csc485/tests/hw10/test_hw10.py.py
@@ -0,0 +1,21 @@
+import pytest
+from Homework10_program import is_it_a_fruit
+
+
+def test_1():
+    assert is_it_a_fruit('apple') == True
+
+def test_2():
+    assert is_it_a_fruit('pear') == True
+
+def test_3():
+    assert is_it_a_fruit('BAnanna') == False
+
+def test_4():
+    assert is_it_a_fruit('') == False
+
+def test_4():
+    assert is_it_a_fruit('cherry') == False
+
+
+
