0b34a26b4f3a958c3699eb07a6767c2585581b67
John-Dublanyk <jdubl001@plattsburgh.edu>
2023-10-09 17:15:04 -0400

    Initial commit

---
+++ projects/hw10/fruit_query.py
@@ -0,0 +1,8 @@
+def is_it_a_fruit(fruit_candidate):
+    fruits = ['apple', 'pear', 'bananna', 'grape']
+
+    if fruit_candidate in fruits:
+        return True
+    else:
+        return False
+print('hi')
