1f4349563d73fea5e599f192359680a0ef9b080f
John-Dublanyk <jdubl001@plattsburgh.edu>
2023-10-09 19:39:04 -0400

    hw12 commit

--- tests/tests_hw12/test_hw12.py
+++ tests/tests_hw12/test_hw12.py
@@ -2,9 +2,9 @@ import pytest
 from projects.hw12.hw12 import compute_complexity
 
 @pytest.mark.parametrize('key', [
-    ('password123', 0.0),
-    ('p@ssw0rd!', 14.285714285714286),
-    ('!@#$', 100.0),
+    ('johndub123', 0.0),
+    ('j0hn@ub1!', 14.285714285714286),
+    ('!^^&', 100.0),
     ('', 0.0),
 ])
 def test_key(key):
