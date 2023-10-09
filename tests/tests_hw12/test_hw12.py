f0bea4aa7271f8bac82d8e04702e6266b654aa2a
John-Dublanyk <143518107+John-Dublanyk@users.noreply.github.com>
2023-10-09 19:49:00 -0400

    test_hw12.py

--- tests/tests_hw12/test_hw12.py
+++ tests/tests_hw12/test_hw12.py
@@ -1,6 +1,9 @@
 import pytest
 from projects.hw12.hw12 import compute_complexity
-
+'''
+1st- no special characters, 2nd- two special characters, 
+3rd- only special characters, 4th- empty string
+'''
 @pytest.mark.parametrize('key', [
     ('johndub123', 0.0),
     ('j0hn@ub1!', 14.285714285714286),
